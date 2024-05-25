f823ae7e132eb2aeb4752bb4a493e1e7cb850569
German Magdiel Alania Tito <germanmagdielalaniatito@gmail.com>
2024-05-24 21:33:13 -0500

    Metodos y funciones

---
+++ Sesion01/Persona.cs
@@ -0,0 +1,28 @@
+﻿namespace Sesion01;
+
+public class Persona
+{
+    // Esto es una propiedad
+    public string Nombre { get; set; }
+
+    public string Apellido { get; set; } = string.Empty;
+
+    public void MostrarNombreCompleto()
+    {
+        Console.WriteLine($"{Nombre} {Apellido}");
+    }
+    // override significa sobreescribir
+    public override string ToString()
+    {
+        return $"El nombre completo es {Nombre} {Apellido}";
+    }
+
+    //Constructor
+    public Persona()
+    {
+        Nombre = string.Empty;
+    }
+
+}
+
+
